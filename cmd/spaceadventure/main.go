bb9991d7e1adac9b4fd0f661a576a7c12367c8df
Carson-Key <keyc@oregonstate.edu>
2019-11-25 15:33:11 -0800

    Refactor Planet/Planets Struct

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -3,5 +3,21 @@ package main
 import "github.com/carson-key/go-spaceadventure-walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.Start()
+	spaceadventure.Start(
+		spaceadventure.PlanetarySystem{
+			Name: "Solar System", Planets: []spaceadventure.Planet{
+				spaceadventure.Plant{
+					"Earth",
+					"Home to life"
+				}
+			},
+		}
+	)
+}
+
+func mockPlanets() spaceadventure.Plant[] {
+	return []spaceadventure.Planet{
+		Planet{"Earth", "Life Planet"},
+		Planet{"Mars", "Desolate Planet"}
+	}
 }
--- internal/spaceadventure/spaceadventure.go
+++ internal/spaceadventure/spaceadventure.go
@@ -2,15 +2,15 @@ package spaceadventure
 
 import "fmt"
 
-func Start() {
-  printWelcome()
+func Start(planetarySystem PlanetarySystem) {
+  printWelcome(planetarySystem)
 	printGreeting(responseToPrompt("What is your name?"))
 	fmt.Println("Let's go on an adventure!")
-	travel()
+	travel(promptForTravelToDestination("Shall I randomly choose a planet for you to visit? (Y or N)"))
 }
 
-func printWelcome() {
-	fmt.Println("Welcome to the Solar System!")
+func printWelcome(planetarySystem PlanetarySystem) {
+	fmt.Println("Welcome to the %s!", planetarySystem.Name)
 	fmt.Println("There are 8 planets to explore.")
 }
 
@@ -18,19 +18,27 @@ func printGreeting(name string) {
 	fmt.Printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Siri.\n", name)
 }
 
-func travel() {
+func travel(randomDestination bool) {
+  if (randomDestination) {
+    travelToRandomPlanet()
+  } else {
+    travelToPlanet(responseToPrompt("Name the planet you would like to visit."))
+  }
+}
+
+func promptForTravelToDestination(prompt string) bool {
 	var choice string
 	for choice != "Y" && choice != "N" {
-		choice = responseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
+		choice = responseToPrompt(prompt)
 		if choice == "Y" {
-			travelToRandomPlanet()
+			return true
 		} else if choice == "N" {
-			planetName := responseToPrompt("Name the planet you would like to visit.")
-			travelToPlanet(planetName)
+			return false
 		} else {
 			fmt.Println("Sorry, I didn't get that.")
 		}
 	}
+  return false
 }
 
 func responseToPrompt(prompt string) string {
